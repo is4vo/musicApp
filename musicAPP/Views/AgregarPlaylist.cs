af2e6b91fa5bbe99835f97746a9a6870669772e5
Camila <camila.hughlaurie@gmail.com>
2021-06-21 11:52:16 -0400

    Comentarios

--- musicAPP/Views/AgregarPlaylist.cs
+++ musicAPP/Views/AgregarPlaylist.cs
@@ -12,6 +12,10 @@ using musicAPP.Controllers;
 
 namespace musicAPP
 {
+    /// <summary>
+    /// Formulario que funciona como modal para que el usuario complete los datos necesario 
+    /// para agregar una nueva playlist, los cuales son: nombre y archivos de canciones.
+    /// </summary>
     public partial class AgregarPlaylist : Form
     {
         private bool SubirCancionesClickeado = false;
@@ -20,6 +24,12 @@ namespace musicAPP
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Evento que se activa cuando se clickea el boton "subir canciones" de la interfaz.
+        /// Abre un seleccionador de archivos y permite seleccionar uno o mas archivos.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SubirCancionesPlaylist_Click(object sender, EventArgs e)
         {
             openFileDialog1.Multiselect = true;
@@ -31,8 +41,16 @@ namespace musicAPP
             SubirCancionesClickeado = true;
         }
 
+        /// <summary>
+        /// Evento que se activa cuando se clickea el boton "aceptar" de la interfaz.
+        /// Procede a añadir la nueva playlist, con el nombre asignado en el input y 
+        /// los archivos seleccionados en el openFileDialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CrearPlaylist_Click(object sender, EventArgs e)
         {
+            /* Antes de añadir la playlist se verifica que el nombre no venga vacio o lleno de espacios y que al menos se haya seleccionado un archivo */
             if (!string.IsNullOrWhiteSpace(inputNombrePlaylist.Text) && SubirCancionesClickeado && openFileDialog1.FileNames.Length > 0 && !openFileDialog1.FileName.Equals("openFileDialog1"))
             {
                 PlayListController.AddPlayList(inputNombrePlaylist.Text);
--- musicAPP/Views/MusicApp.cs
+++ musicAPP/Views/MusicApp.cs
@@ -19,48 +19,56 @@ namespace musicAPP
             InitializeComponent();
         }
 
-        /* Abrir buscador de archivos */
+        
+        /// <summary>
+        /// Evento que se activa cuando el boton "canciones" es clickeado.
+        /// Se encarga de llenar con información de las canciones los paneles "tituloSeccion" y "seccion" de la interfaz.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void botonCanciones_Click(object sender, EventArgs e)
         {
             iniciarlizarPanelesCanciones();
         }
 
+        /// <summary>
+        /// Limpia los paneles iniciales de la interfaz y los completa con la información a mostrar de todas las canciones guardadas.
+        /// </summary>
         private void iniciarlizarPanelesCanciones()
         {
             tituloSeccion.Controls.Clear();
             crearCuadroSeccionCanciones();
             seccion.Controls.Clear();
+            /* En el panel "seccion" se crea un panel con información por cada canción*/
             foreach (var cancion in CancionController.GetList())
             {
                 seccion.Controls.Add(crearCuadroCancion(cancion.Titulo, cancion.Artistas, cancion.Duracion, cancion.Album, cancion.Ubicacion, true));
             }
         }
 
+        /// <summary>
+        /// Evento que se realiza al iniciar la aplicación.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MusicApp_Load(object sender, EventArgs e)
         {
+            /* Se pone la imagen del logo en el espacio asignado. */
             Logo.Image = Image.FromFile(System.IO.Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\musicApp\\Images\\logo.png");
             iniciarlizarPanelesPlaylist();
         }
 
-        /* Agregar canciones a una Lista de reproduccion de Windows Media Player*/
-        private void CrearLista(object sender, EventArgs e)
-        {
-            WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("La Lista");
-            WMPLib.IWMPMedia media;
-            openFileDialog1.Multiselect = true;
-            openFileDialog1.Filter = "Archivos de Audio|*.wav;*.mp3;*.alac;*.ALAC;*.WAV;*.AAC;*.MP3;"; // Valida tipo de archivo
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                foreach (string file in openFileDialog1.FileNames)
-                {
-                    media = axWindowsMediaPlayer1.newMedia(file);
-                    playlist.appendItem(media);
-                }
-            }
-            axWindowsMediaPlayer1.currentPlaylist = playlist;
-            axWindowsMediaPlayer1.Ctlcontrols.play();
-        }
-
+        /// <summary>
+        /// Se genera el panel que contiene la información de la cancion como tambien botones para eliminar y reproducir.
+        /// Este panel se divide en dos sub paneles, uno que contiene la información y otro que contiene los botones.
+        /// </summary>
+        /// <param name="titulo"> El titulo de la canción </param>
+        /// <param name="artista"> El artista que compone la canción </param>
+        /// <param name="duracion"> La duración en segundos de la canción </param>
+        /// <param name="album"> El album al cual pertenece la canción </param>
+        /// <param name="ubicacion"> La path en la cual se encuentra la canción dentro del computador </param>
+        /// <param name="botonesActivos"> Indicador de si los botonos deben aparecer o no (en caso de listar canciones de playlista se esconden) </param>
+        /// <returns> El panel con todos los componentes e información necesaria </returns>
         private FlowLayoutPanel crearCuadroCancion(string titulo, string artista, double duracion, string album, string ubicacion, Boolean botonesActivos)
         {
             FlowLayoutPanel p = new FlowLayoutPanel();
@@ -73,6 +81,15 @@ namespace musicAPP
             return p;
         }
 
+        /// <summary>
+        /// Crear un sub panel que se agrega dentro del panel general que es creado por cada canción.
+        /// Este panel solo contiene la información de la canción.
+        /// </summary>
+        /// <param name="titulo"> El titulo de la canción </param>
+        /// <param name="artista"> El artista que compone la canción </param>
+        /// <param name="duracion"> La duración en segundos de la canción </param>
+        /// <param name="album"> El album al cual pertenece la canción </param>
+        /// <returns> El panel con todos los componentes e información necesaria </returns>
         private FlowLayoutPanel crearCuadroInformacion(string titulo, string artista, double duracion, string album)
         {
             FlowLayoutPanel p2 = new FlowLayoutPanel();
@@ -105,15 +122,23 @@ namespace musicAPP
             return p2;
         }
 
+        /// <summary>
+        /// Crear un sub panel que se agrega dentro del panel general que es creado por cada canción.
+        /// Este panel solo contiene los botones eliminar y reproducir que van dentro del panel de cada canción.
+        /// </summary>
+        /// <param name="ubicacion"> La path en la cual se encuentra la canción dentro del computador </param>
+        /// <returns> El panel con todos los componentes e información necesaria </returns>
         private FlowLayoutPanel crearCuadroBotones(string ubicacion)
         {
             FlowLayoutPanel p3 = new FlowLayoutPanel();
             Button b = crearBotonReproducir(7, 105, 30);
             Button b2 = crearBotonEliminar(7, 105, 30);
+            /*Al clickear este boton se procede a eliminar la canción.*/
             b2.Click += (object se, EventArgs ee) =>
             {
                 eliminarCancion(ubicacion);
             };
+            /*Al clickear este boton se procede a reproducir la canción.*/
             b.Click += (object se, EventArgs ee) =>
             {
                 reproducirCancion(ubicacion);
@@ -124,6 +149,12 @@ namespace musicAPP
             return p3;
         }
 
+        /// <summary>
+        /// Crea un panel que contiene un texto y un boton, 
+        /// el texto indica cuantas canciones guardadas hay en total y 
+        /// el boton se utiliza para añadir mas canciones.
+        /// Este panel se agrega dentro del panel "sección" de la interfaz.
+        /// </summary>
         private void crearCuadroSeccionCanciones()
         {
             FlowLayoutPanel p4 = new FlowLayoutPanel();
@@ -147,6 +178,7 @@ namespace musicAPP
             b.Size = new Size(156, 39);
             b.FlatAppearance.BorderSize = 0;
             t.SetToolTip(b, "Agregar nueva canción");
+            /*Al clickear este boton se procede a añadir una nueva canción.*/
             b.Click += (object se, EventArgs ee) =>
             {
                 agregarCancion();
@@ -156,6 +188,12 @@ namespace musicAPP
             tituloSeccion.Controls.Add(p5);
         }
 
+        /// <summary>
+        /// Se utiliza para guardar una nueva canción.
+        /// Abre el seleccionador de archivos y permite elegir un archivo,
+        /// luego el archivo se guarda y se actualizan los paneles de la interfaz 
+        /// que muestran info de las canciones.
+        /// </summary>
         public void agregarCancion()
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -165,16 +203,26 @@ namespace musicAPP
             iniciarlizarPanelesCanciones();
         }
 
+        /// <summary>
+        /// Se utiliza para borrar una canción.
+        /// Borra la canción mediante su identificador (path) 
+        /// y actualiza lo necesario para refrescar el cambio en la interfaz.
+        /// </summary>
+        /// <param name="ubicacion"> La path en la cual se encuentra la canción dentro del computador </param>
         public void eliminarCancion(string ubicacion)
         {
             CancionController.removeFile(ubicacion);
             iniciarlizarPanelesCanciones();
+
+            /* Se revisa si alguna playlist quedo vacia luego de borrar dicha canción*/
             List<string> listasParaBorrar = new List<string>();
             foreach (var playlistName in PlayListController.getAllPlayList())
             {
                 if (PlayListController.getPlayList(playlistName).Count < 1)
                     listasParaBorrar.Add(playlistName);
             }
+
+            /* Si alguna playlist quedo vacia se borra */
             foreach (var listaName in listasParaBorrar)
             {
                 PlayListController.removePlayList(listaName);
@@ -183,6 +231,13 @@ namespace musicAPP
             eliminarCancionDeWindowsMediaPlayer(ubicacion);
         }
 
+        /// <summary>
+        /// Manda a reproducir una canción al windows media player.
+        /// La reproduce creando una lista con un archivo media y
+        /// mandando a reproducir dicha lista, el archivo se crea 
+        /// mediante la path de la canción.
+        /// </summary>
+        /// <param name="ubicacion"> La path en la cual se encuentra la canción dentro del computador </param>
         public void reproducirCancion(string ubicacion)
         {
             WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("cancion");
@@ -193,22 +248,40 @@ namespace musicAPP
             axWindowsMediaPlayer1.Ctlcontrols.play();
         }
 
+        /// <summary>
+        /// Evento que se activa cuando se presiona el boton "+" que se encuentra al lado de playlist.
+        /// Se utiliza para añadir una nueva playlist y se actualiza lo necesario para que el cambio
+        /// se refleje en la interfaz.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void agregarPlaylist_Click(object sender, EventArgs e)
         {
+            /* Se abre el form "AgregarPlaylist" que funciona como modal para que el usuario complete los datos de la playlist a agregar */
             AgregarPlaylist f = new AgregarPlaylist();
             f.ShowDialog();
             iniciarlizarPanelesPlaylist();
         }
 
+        /// <summary>
+        /// Limpia el panel "seccionPlaylist" y lo completa con información de las playlist guardadas.
+        /// </summary>
         private void iniciarlizarPanelesPlaylist()
         {
             seccionPlaylist.Controls.Clear();
+            /* Por cada playlist se genera un boton con su nombre */
             foreach (var playlist in PlayListController.getAllPlayList())
             {
                 seccionPlaylist.Controls.Add(crearPanelPlaylist(playlist));
             }
         }
 
+        /// <summary>
+        /// Se crear un boton que lleva el nombre de una playlist.
+        /// Se utiliza para ser agregado dentro del panel "seccionPlaylist" de la interfaz.
+        /// </summary>
+        /// <param name="nombre"> El nombre de la playlist </param>
+        /// <returns> Un boton con el nombre de la playlist </returns>
         private Button crearPanelPlaylist(string nombre)
         {
             Button playlist = new Button();
@@ -218,6 +291,7 @@ namespace musicAPP
             playlist.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
             playlist.ForeColor = Color.White;
             playlist.TextAlign = ContentAlignment.MiddleLeft;
+            /* Al clickear este boton se muestra toda la información de la playlist en los paneles "tituloSeccion" y "seccion" de la interfaz */
             playlist.Click += (object se, EventArgs ee) =>
             {
                 mostrarPlaylist(nombre);
@@ -225,11 +299,16 @@ namespace musicAPP
             return playlist;
         }
 
+        /// <summary>
+        /// Se encarga de llenar con información de una playlist los paneles "tituloSeccion" y "seccion" de la interfaz.
+        /// </summary>
+        /// <param name="nombre"> El nombre de la playlist </param>
         private void mostrarPlaylist(string nombre)
         {
             tituloSeccion.Controls.Clear();
             crearCuadroSeccionPlaylist(nombre);
             seccion.Controls.Clear();
+            /* Lista las canciones de la playlist */
             foreach (var playlist in PlayListController.getPlayList(nombre))
             {
                 foreach (var cancion in CancionController.GetList())
@@ -240,6 +319,13 @@ namespace musicAPP
             }
         }
 
+        /// <summary>
+        /// Crea un boton que dice reproducir y tiene un icono acorde a la descripción.
+        /// </summary>
+        /// <param name="tamanoLetra"> El tamaño de letra que tendra el boton </param>
+        /// <param name="ancho"> El ancho del boton </param>
+        /// <param name="alto"> El alto del boton </param>
+        /// <returns> El boton con creado con las medidas solicitadas </returns>
         private Button crearBotonReproducir(int tamanoLetra, int ancho, int alto)
         {
             Button reproducir = new Button();
@@ -254,6 +340,13 @@ namespace musicAPP
             return reproducir;
         }
 
+        /// <summary>
+        /// Crea un boton que dice eliminar y tiene un icono acorde a la descripción.
+        /// </summary>
+        /// <param name="tamanoLetra"> El tamaño de letra que tendra el boton </param>
+        /// <param name="ancho"> El ancho del boton </param>
+        /// <param name="alto"> El alto del boton </param>
+        /// <returns> El boton con creado con las medidas solicitadas </returns>
         private Button crearBotonEliminar(int tamanoLetra, int ancho, int alto)
         {
             Button eliminar = new Button();
@@ -268,6 +361,11 @@ namespace musicAPP
             return eliminar;
         }
 
+        /// <summary>
+        /// Crea un panel que se compone por información de la playlist mas botonos para eliminar o reproducir dicha playlist.
+        /// La información a mostrar de la playlist es el nombre, numero de canciones y tiempo de duración total.
+        /// </summary>
+        /// <param name="nombre"> El nombre de la playlist </param>
         private void crearCuadroSeccionPlaylist(string nombre)
         {
             FlowLayoutPanel panelTitulos = new FlowLayoutPanel();
@@ -296,10 +394,12 @@ namespace musicAPP
             panelTitulos.Controls.Add(panel2);
             Button botonReproducirPlaylist = crearBotonReproducir(9, 120, 40);
             Button botonEliminarPlaylist = crearBotonEliminar(9, 120, 40);
+            /* Al clickear este boton se reproduce la playlist */
             botonReproducirPlaylist.Click += (object se, EventArgs ee) =>
             {
                 reproducirPlaylist(nombre);
             };
+            /* Al clickear este boton se elimina la playlist */
             botonEliminarPlaylist.Click += (object se, EventArgs ee) =>
             {
                 eliminarPlaylist(nombre);
@@ -311,6 +411,12 @@ namespace musicAPP
             tituloSeccion.Controls.Add(panel4);
         }
 
+        /// <summary>
+        /// Se encarga de reproducir una playlist, para ello
+        /// se recorre la lista de canciones y se genera una 
+        /// lista de media para mandar a reproducir dentro del window media player.
+        /// </summary>
+        /// <param name="nombre"> El nombre de la playlist </param>
         private void reproducirPlaylist(string nombre)
         {
             WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist(nombre);
@@ -324,6 +430,12 @@ namespace musicAPP
             axWindowsMediaPlayer1.Ctlcontrols.play();
         }
 
+        /// <summary>
+        /// Se encarga de eliminar una playlist, se
+        /// elimina la playlist mediante su identificador (nombre) y 
+        /// se actualiza la interfaz para ver el cambio.
+        /// </summary>
+        /// <param name="nombre"> El nombre de la playlist </param>
         private void eliminarPlaylist(string nombre)
         {
             PlayListController.removePlayList(nombre);
@@ -333,6 +445,13 @@ namespace musicAPP
             eliminarPlaylistDeWindowsMediaPlayer(nombre);
         }
 
+        /// <summary>
+        /// Se encarga de obtener el tiempo total que tardara una 
+        /// playlist en reproducirse en base a la suma de los tiempos
+        /// de las canciones que contiene.
+        /// </summary>
+        /// <param name="nombre"> El nombre de la playlist </param>
+        /// <returns> Un double que indica el tiempo total en segundos </returns>
         private double getTiempoTotalPlaylist(string nombre)
         {
             double tiempoTotal = 0;
@@ -347,6 +466,12 @@ namespace musicAPP
             return tiempoTotal;
         }
 
+        /// <summary>
+        /// Se utiliza para detener la cancion sonando en el windows media player si es que esta canción fue eliminada.
+        /// Se toma la playlist que esta sonando actualmente y se elimina el media/archivo de la playlist si este 
+        /// coincide con la canción eliminada (se compara con la path).
+        /// </summary>
+        /// <param name="ubicacion"> La path en la cual se encuentra la canción dentro del computador </param>
         private void eliminarCancionDeWindowsMediaPlayer(string ubicacion)
         {
             WMPLib.IWMPPlaylist pl = axWindowsMediaPlayer1.currentPlaylist;
@@ -357,14 +482,21 @@ namespace musicAPP
             }
         }
 
+        /// <summary>
+        /// Se utiliza para detener la playlist sonando en el windows media player si que esta playlist fue eliminada.
+        /// Se toma la playlist que esta sonando actualmente y se compara con el nombre de la playlist eliminada, si 
+        /// coindice entonces se cambia la playlist por otra vacia para detener la reproducción.
+        /// </summary>
         private void eliminarPlaylistDeWindowsMediaPlayer(string nombre)
         {
-            WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("cancion");
-            WMPLib.IWMPMedia media;
-            media = axWindowsMediaPlayer1.newMedia(null);
-            playlist.appendItem(media);
-            axWindowsMediaPlayer1.currentPlaylist = playlist;
-            axWindowsMediaPlayer1.Ctlcontrols.play();
+            if (axWindowsMediaPlayer1.currentPlaylist.name.Equals(nombre)){
+                WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("cancion");
+                WMPLib.IWMPMedia media;
+                media = axWindowsMediaPlayer1.newMedia(null);
+                playlist.appendItem(media);
+                axWindowsMediaPlayer1.currentPlaylist = playlist;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
         }
     }   
 }
